6cd35731a2dae7574afdb6cc93df0012c3be2522
sokaman <skmdev29@gmail.com>
2018-07-17 23:08:54 +0800

    export interface IContext

--- decorators/graphql.ts
+++ decorators/graphql.ts
@@ -2,15 +2,28 @@ import Koa from 'koa';
 import compose from 'koa-compose';
 import { isArray } from '../utils';
 
+export interface IContext extends Koa.Context {
+  graphql: {
+    root: any;
+    args: any;
+    info: any;
+  };
+}
+
 export const Graphql = (target: any, name: string, descriptor: any): any => {
   const middleware = isArray(target[name]);
-  descriptor.value = async (root: any, args: any, context: Koa.Context, info: any) => {
+  descriptor.value = async (
+    root: any,
+    args: any,
+    context: Koa.Context,
+    info: any,
+  ) => {
     context.graphql = {
       root,
       args,
       info,
     };
-    compose<Koa.Context>(middleware)(context);
+    compose<IContext>(middleware)(<IContext>context);
     return context.body;
   };
 };

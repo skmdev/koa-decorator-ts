28e66fbcc09029e60423591d981d6a8caa50e659
sokaman <skmdev29@gmail.com>
2018-09-20 00:17:52 +0800

    Declare the graphql in koa context

--- index.ts
+++ index.ts
@@ -1,2 +1,14 @@
+declare module 'koa' {
+  interface Context {
+    graphql?: {
+      args: {
+        [key: string]: any;
+      };
+    };
+    root: any;
+    info: any;
+  }
+}
+
 export * from './decorators/router';
 export * from './decorators/graphql';
